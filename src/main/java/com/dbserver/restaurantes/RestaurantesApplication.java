14bda9d56b4f0876cd50ad471f27997f7a77c59c
brunomdrrosa <brunomdr46@gmail.com>
2022-03-15 11:35:52 -0300

    Senhas criptografadas

--- src/main/java/com/dbserver/restaurantes/RestaurantesApplication.java
+++ src/main/java/com/dbserver/restaurantes/RestaurantesApplication.java
@@ -2,8 +2,9 @@ package com.dbserver.restaurantes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class RestaurantesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/dbserver/restaurantes/services/UserServices.java
+++ src/main/java/com/dbserver/restaurantes/services/UserServices.java
@@ -3,6 +3,8 @@ package com.dbserver.restaurantes.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,16 +16,25 @@ public class UserServices {
 
 	@Autowired
 	private UserRepository repository;
+
+	PasswordEncoder passwordEncoder;
+
 	
+	public UserServices(UserRepository userRepository) {
+		this.passwordEncoder = new BCryptPasswordEncoder();
+	}
+
 	@SuppressWarnings("rawtypes")
 	@Transactional(readOnly = true)
 	public List<User> findAllUsers(List list) {
 		List<User> result = repository.findAll();
 		return result;
 	}
-	
+
 	@Transactional
 	public User addUser(User newUser) {
+		String encodedPassword = this.passwordEncoder.encode(newUser.getPassword());
+		newUser.setPassword(encodedPassword);
 		return repository.saveAndFlush(newUser);
 	}
 }
