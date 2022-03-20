89672e041cd0d2b2ba83f7c3e355595150ac6c81
brunomdrrosa <brunomdr46@gmail.com>
2022-03-19 23:47:20 -0300

    Removendo dependências não utilizadas

--- src/main/java/com/dbserver/restaurantes/RestaurantsApplication.java
+++ src/main/java/com/dbserver/restaurantes/RestaurantsApplication.java
@@ -4,15 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@EnableWebMvc
 @EnableScheduling
-public class RestaurantesApplication {
+public class RestaurantsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestaurantesApplication.class, args);
+		SpringApplication.run(RestaurantsApplication.class, args);
 	}
 	
 }
